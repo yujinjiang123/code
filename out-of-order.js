4f4d67a500e01017d2d3d65a4044c9b8e140698d
yujinjiang <964494258@qq.com>
2021-02-19 15:45:14 +0800

    乱序

---
+++ out-of-order.js
@@ -0,0 +1,9 @@
+const arr=[1,2,3,4,5];
+
+function shuffle(arr){
+    for(let i=arr.length-1;i>=0;i--){
+        let j=Math.floor(Math.random()*i);
+        [arr[i],arr[j]]=[arr[j],arr[i]];
+    }
+}
+
