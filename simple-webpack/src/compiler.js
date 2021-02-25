e58b171065767e7c5f4795892fd59d60c47723df
yujinjiang <964494258@qq.com>
2021-02-25 16:31:23 +0800

    简易版webpack实现

---
+++ simple-webpack/src/compiler.js
@@ -0,0 +1,17 @@
+class Compiler{
+    constructor(options) {
+        const {entry,output}=options;
+        this.entry=entry;
+        this.output=output;
+        this.modules=[];
+    }
+    
+
+    run(){
+
+    }
+
+    generate(){
+        
+    }
+}
