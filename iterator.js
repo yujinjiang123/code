bd5ba712378660a5e869410f397bf939ad374118
yujinjiang <964494258@qq.com>
2021-03-03 00:51:57 +0800

    使用Gengrator实现对象的解析

--- iterator.js
+++ iterator.js
@@ -37,3 +37,20 @@ for(let i of counter){
     console.log(i);
 }
 
+
+const obj={
+    name:'yujinjiang',
+    age:21,
+
+    [Symbol.iterator]:function *(){
+        const keys=Object.keys(this);
+        for(let key of keys){
+            yield [key,this[key]];
+        }
+    }
+}
+
+for(let [key,val] of obj){
+    console.log(key,val);
+}
+console.log(Array.from(obj))
