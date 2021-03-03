f94afbf5e9181e715ec6e3cebd01c4f5fc73da14
yujinjiang <964494258@qq.com>
2021-03-03 19:19:45 +0800

    期约进度通知

---
+++ Promise/TrackblePromise.js
@@ -0,0 +1,30 @@
+class TrackablePromise extends Promise {
+  constructor(executor) {
+    const notifyHandlers = [];
+    super((resolve, reject) => {
+      return executor(resolve, reject, (status) => {
+        notifyHandlers.map((handler) => handler(status));
+      });
+    });
+    this.notifyHandlers = notifyHandlers;
+  }
+
+  notify(notifyHandler) {
+    this.notifyHandlers.push(notifyHandler);
+    return this;
+  }
+}
+
+const p = new TrackablePromise((resolve, reject, notify) => {
+  function countdown(x) {
+    if (x > 0) {
+      notify(`${20 * x}% remaining`);
+      setTimeout(() => countdown(x - 1), 1000);
+    } else {
+      resolve();
+    }
+  }
+  countdown(5);
+});
+p.notify(x=>setTimeout(console.log,0,'progress:',x)).notify((x)=>setTimeout(console.log,0,'a:',x));
+p.then(()=>setTimeout(console.log,0,'completed'));
